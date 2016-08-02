cd8ad774363026d2e450012003c73b66de7ece2d
Ben Petro <bpetro@feet.shoes>
2016-08-02 23:43:00 +0000

    config fix

--- app/helpers/universal/configurables_helper.rb
+++ app/helpers/universal/configurables_helper.rb
@@ -5,7 +5,7 @@ module Universal
       keys = UNIVERSAL_CONFIGS[model.class.name.demodulize.to_s.underscore.downcase.to_sym]
       #logger.debug keys
       if keys and !section.blank?
-        keys = keys[section.to_s.downcase]
+        keys = keys[section.to_s.downcase.underscore]
       end
       puts keys
       render partial: '/universal/configurables/easy_config', locals: {model: model, keys: keys}
