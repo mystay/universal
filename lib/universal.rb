152028b38465021022e4696724ba285dd5a1b1e9
Ben Petro <bpetro@feet.shoes>
2016-08-03 00:19:24 +0000

    fixed concern directory

--- lib/universal.rb
+++ lib/universal.rb
@@ -2,7 +2,7 @@ require "universal/engine"
 require "universal/extensions"
 require "universal/configuration"
 Gem.find_files("universal/models/*.rb").each { |path| require path }
-Gem.find_files("../app/models/concerns/*.rb").each { |path| require path }
+Gem.find_files("../app/models/universal/concerns/*.rb").each { |path| require path }
 
 module Universal
 
