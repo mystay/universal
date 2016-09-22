729f9e99b4c02e05e13ac3b5cc5659c0a8a865a1
Ben Petro <bpetro@feet.shoes>
2016-09-22 07:08:20 +0000

    scope fix

--- app/models/universal/concerns/scoped.rb
+++ app/models/universal/concerns/scoped.rb
@@ -10,7 +10,7 @@ module Universal
         def prepare_scope
           if !UniversalCrm::Configuration.scope_class.blank?  
             belongs_to :scope, polymorphic: true
-            scope :scoped_to, ->(model){where(scope_id: (model.nil? ? nil : model.id), scope_type: (model.nil? ? nil : model.class))}
+            scope :scoped_to, ->(model){where(scope_id: (model.nil? ? nil : model.id), scope_type: (model.nil? ? nil : model.class.to_s))}
             
           end
         end
