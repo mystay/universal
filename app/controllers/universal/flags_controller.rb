77ccb55f7b6b28a8bced0413915bb4c14ea62a12
Ben Petro <benpetro@gmail.com>
2017-08-31 05:07:45 +0000

    flag toggle fix

--- app/controllers/universal/flags_controller.rb
+++ app/controllers/universal/flags_controller.rb
@@ -19,15 +19,21 @@ module Universal
     end
 
     def toggle
-      @model = params[:model_type].classify.constantize.find params[:model_id]
-      if @model.flagged_with?(params[:flag])
-        @add = false
-        @model.remove_flag!(params[:flag])
-        @model.log!("Flag removed: #{params[:flag].titleize}", current_user)
-      else
-        @add = true
-        @model.flag!(params[:flag], current_user)
-        @model.log!("Flag added: #{params[:flag].titleize}", current_user)
+      if !params[:model_type].blank? and !params[:model_id].blank?
+        @model = params[:model_type].classify.constantize.find params[:model_id]
+      elsif !params[:subject_type].blank? and !params[:subject_id].blank?
+        @model = params[:subject_type].classify.constantize.find params[:subject_id]
+      end
+      if !@model.nil?
+        if @model.flagged_with?(params[:flag])
+          @add = false
+          @model.remove_flag!(params[:flag])
+          @model.log!("Flag removed: #{params[:flag].titleize}", current_user)
+        else
+          @add = true
+          @model.flag!(params[:flag], current_user)
+          @model.log!("Flag added: #{params[:flag].titleize}", current_user)
+        end
       end
       xhr?
     end
